df5d362bd659ce8f1204af04b49b0c9493a418fe
linwenjun <linwenjun@163.com>
2019-12-19 02:11:52 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# 使用方式
+
+* `./gradlew clean build`
+  * 如果你使用Windows，这一步应该是 `gradlew.bat clean build`
+
+此时你应该看到成功的build：
+
+```
+$ ./gradlew clean build
+
+BUILD SUCCESSFUL in 1s
+4 actionable tasks: 4 executed
+```
+
+用Intellij IDEA或Eclipse或者你习惯的任何IDE（但是我强烈建议不要使用IDEA或Eclipse之外的其他IDE）导入工程，运行全部测试，你应该看到绿条。
+
+从这里开始你的Dojo练习。
---
+++ src/main/java/com/thoughtworks/refactor/Rover.java
@@ -0,0 +1,79 @@
+package com.thoughtworks.refactor;
+
+import java.util.Arrays;
+
+import static java.lang.Integer.valueOf;
+
+public class Rover {
+    private Integer x;
+    private Integer y;
+    private String direction;
+
+    public Rover(String initialStatus) {
+        String[] status = initialStatus.split(" ");
+        x = valueOf(status[0]);
+        y = valueOf(status[1]);
+        direction = status[2];
+    }
+
+    public String status() {
+        return x + " " + y + " " + direction;
+    }
+
+    public void on(String demand) {
+        if ("l".equalsIgnoreCase(demand)) {
+            switch (direction) {
+                case "w":
+                    direction = "s";
+                    break;
+                case "s":
+                    direction = "e";
+                    break;
+                case "e":
+                    direction = "n";
+                    break;
+                case "n":
+                    direction = "w";
+                    break;
+            }
+        }
+
+        if ("r".equalsIgnoreCase(demand)) {
+            switch (direction) {
+                case "w":
+                    direction = "n";
+                    break;
+                case "n":
+                    direction = "e";
+                    break;
+                case "e":
+                    direction = "s";
+                    break;
+                case "s":
+                    direction = "w";
+                    break;
+            }
+        }
+
+        if ("f".equalsIgnoreCase(demand)) {
+            switch (direction) {
+                case "w":
+                    x--;
+                    break;
+                case "s":
+                    y--;
+                    break;
+                case "e":
+                    x++;
+                    break;
+                case "n":
+                    y++;
+                    break;
+            }
+        }
+    }
+
+    void execute(String statement) {
+        Arrays.asList(statement.split("(?!^)")).forEach(cmd -> on(cmd));
+    }
+}
